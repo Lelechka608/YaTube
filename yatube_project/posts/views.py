668407d3d9817c0d4c20fb7648ff10eef1b913a0
Alena Meshkova <alenka.meshkova@yandex.ru>
2022-10-16 18:41:23 +0300

    Добавлене views и связей

--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse('Главная страница')
 
-# Create your views here.
+
+def group_posts(request, slug):
+    return HttpResponse('Группы постов {slug}')
+
+
+
+
